7159f05b3a4e18f1dca374743a148ceba0750ccc
summerfranks <bahamasfranks@gmail.com>
2024-11-12 14:41:29 -0700

    minor tweaks to sf2 program flow

--- include/arm/smartfusion/sf2_debug.sh
+++ include/arm/smartfusion/sf2_debug.sh
@@ -9,11 +9,11 @@ export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:$INCLUD
 pushd $INCLUDE_LOCAL/arm/openocd/fpserver 
 pwd
 gnome-terminal -- bash -c ./fpServer
-xfce4-terminal -x ./fpServer
+#xfce4-terminal -x ./fpServer
 popd
 
 killall openocd
-export OPENOCD_SCRIPTS="/home/summer/projects/CGraph/firmware/include/arm/openocd/scripts"
+export OPENOCD_SCRIPTS="$INCLUDE_LOCAL/arm/openocd/scripts"
 #~ pushd $INCLUDE_LOCAL/arm/openocd
 gnome-terminal -- bash -c '$INCLUDE_LOCAL/arm/openocd/openocd --command "gdb_port 3333" --command "tcl_port 6666" --command "telnet_port 4444" --command "set DEVICE M2S010" --file board/microsemi-cortex-m3.cfg --command "program $TARGET.elf verify reset"'
 #~ popd
