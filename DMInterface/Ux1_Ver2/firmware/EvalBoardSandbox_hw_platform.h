2f35201633530492cb1fd794c93ccc922d1adb76
Stephen <kaye2@arizona.edu>
2025-02-20 00:11:49 +0000

    Added processor files.

--- DMInterface/Ux1_Ver2/firmware/EvalBoardSandbox_hw_platform.h
+++ DMInterface/Ux1_Ver2/firmware/EvalBoardSandbox_hw_platform.h
@@ -2,7 +2,7 @@
 #define EvalBoardSandbox_HW_PLATFORM_H_
 /*****************************************************************************
 *
-*Created by Microsemi SmartDesign  Fri Jan 31 14:41:09 2025
+*Created by Microsemi SmartDesign  Wed Feb 19 17:00:47 2025
 *
 *Memory map specification for peripherals in EvalBoardSandbox
 */
@@ -11,13 +11,7 @@
 * CM3 subsystem memory map
 * Initiator(s) for this subsystem: CM3 
 *---------------------------------------------------------------------------*/
-#define COREUARTAPB_C0_0                0x50000000U
-#define SPIMASTERPORTS_0                0x50001000U
-#define SPIMASTERPORTS_1                0x50002000U
-#define SPIMASTERPORTS_2                0x50003000U
-#define SPIMASTERPORTS_3                0x50004000U
-#define SPIMASTERPORTS_4                0x50005000U
-#define SPIMASTERPORTS_5                0x50006000U
+#define EVALSANDBOX_MSS_0               0x50000000U
 
 
 #endif /* EvalBoardSandbox_HW_PLATFORM_H_*/
